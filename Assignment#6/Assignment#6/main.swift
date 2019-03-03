6dcb19ff98983853cf56e6854a3a7b3f6dea6f9c
Jackson Kurtz <jkurtz678@gmail.com>
2019-03-03 14:02:14 -0800

    first commit

---
+++ Assignment#6/Assignment#6/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Assignment#6
+//
+//  Created by Jackson Kurtz on 3/3/19.
+//  Copyright © 2019 Jackson Kurtz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
