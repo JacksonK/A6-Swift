3b166fd875559ac651595352bccbb88be3735c1f
Jackson Kurtz <jkurtz678@gmail.com>
2019-03-05 19:28:23 -0800

    divided ExprC and value into their own files

--- Assignment#6/Assignment#6/main.swift
+++ Assignment#6/Assignment#6/main.swift
@@ -9,37 +9,31 @@
 import Foundation
 
 
-class ExprC {
-}
-
-class NumC: ExprC {
-    var num: Float
-    
-    init(num: Float) {
-        self.num = num
-    }
-}
-
-class StringC: ExprC {
-    var str: String
-    
-    init(str: String) {
-        self.str = str
-    }
-}
-
-func interp( exp: ExprC ) {
+func interp( exp: ExprC ) -> Value {
     switch exp {
     case let exp_num as NumC:
         print( exp_num.num )
+        return NumV( num: exp_num.num )
     case let exp_str as StringC:
         print( exp_str.str )
+        return StringV( str: exp_str.str )
+    case let exp_plusC as PlusC:
+        let left = interp(exp: exp_plusC.left) as! NumV
+        let right = interp(exp: exp_plusC.right) as! NumV
+        let result = left.num + right.num
+        print( result )
+        return NumV(num: result)
     default:
         print("type not found")
+        return Value()
     }
 }
 
-interp( exp: NumC(num: 10))
-interp( exp: StringC(str: "test string"))
+//interp( exp: NumC(num: 10))
+//interp( exp: StringC(str: "test string"))
 
+//interp( exp: StringC(str: "test string"))
 
+// (2 + 3) + 5
+//let interp_result = interp( exp: PlusC(left: PlusC(left: NumC(num: 2), right: NumC(num: 3)), right: NumC(num: 5))) as! NumV
+//print("result: ", interp_result.num)
