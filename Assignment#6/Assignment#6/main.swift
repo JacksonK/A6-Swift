d657ccff22b5686fff8370969f6365ae76c9fee6
Jackson Kurtz <jkurtz678@gmail.com>
2019-03-05 17:35:05 -0800

    added basic interp function that supports NumC and StringC

--- Assignment#6/Assignment#6/main.swift
+++ Assignment#6/Assignment#6/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+
+class ExprC {
+}
+
+class NumC: ExprC {
+    var num: Float
+    
+    init(num: Float) {
+        self.num = num
+    }
+}
+
+class StringC: ExprC {
+    var str: String
+    
+    init(str: String) {
+        self.str = str
+    }
+}
+
+func interp( exp: ExprC ) {
+    switch exp {
+    case let exp_num as NumC:
+        print( exp_num.num )
+    case let exp_str as StringC:
+        print( exp_str.str )
+    default:
+        print("type not found")
+    }
+}
+
+interp( exp: NumC(num: 10))
+interp( exp: StringC(str: "test string"))
+
 
